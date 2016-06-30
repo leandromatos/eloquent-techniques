e1ec9c25705ab1ba3ead51564df96983ae542133
Leandro Matos <leandromatos@is-uz.com>
2016-06-30 03:50:46 -0300

    Record model changes

--- app/Document.php
+++ app/Document.php
@@ -12,7 +12,7 @@ class Document extends Model
         parent::boot();
 
         static::updating(function ($document) {
-            $document->adjustments()->attach(Auth::id());
+            $document->storeAdjustments();
         });
     }
 
@@ -20,6 +20,26 @@ class Document extends Model
     {
         return $this->belongsToMany(User::class, 'adjustments')
             ->withTimestamps()
+            ->withPivot(['before', 'after'])
             ->latest('pivot_updated_at');
+    }
+
+    public function storeAdjustments($userId = null, $diff = null)
+    {
+        $userId = $userId ?: Auth::id();
+        $diff   = $diff ?: $this->getAdjustmentsDiff();
+
+        return $this->adjustments()->attach($userId, $diff);
+    }
+
+    public function getAdjustmentsDiff()
+    {
+        $original = $this->fresh()->toArray();
+        $changed  = $this->getDirty();
+
+        $before = json_encode(array_intersect_key($original, $changed));
+        $after  = json_encode($changed);
+
+        return compact('before', 'after');
     }
 }
